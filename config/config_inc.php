5117a43affd833cb06402eb6bb46e11c54ac5662
Jency Software <jencysoftware@gmail.com>
2023-03-16 11:33:13 +0000

    Changes

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'demo_ticket';
+$g_db_username            = 'root';
+$g_db_password            = 'omSaiRam@123';
+
+$g_default_timezone       = 'UTC';
+
+$g_crypto_master_salt     = 'm8NFTa2ifWb5VVFHclbhzr3QOCrdrqRD4wzc8GRGovY=';
